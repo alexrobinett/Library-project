29cd9f34236b280469404653c36c2929018795cf
robinetta <alxr19@gmail.com>
2022-11-08 23:59:35 -0500

    add book functionality

--- js/app.js
+++ js/app.js
@@ -1,42 +1,82 @@
-// const bookTitle = document.getElementById('booktitle')
-// const bookAuthor = document.getElementById('author')
-// const bookPages = document.getElementById('numpages')
-// const isBookRead = document.getElementById('readbook')
-let newbooktitleTest = "alex's Awesome Book"
-const BookContainer = document.getElementsByClassName('books-container')
+const BookFormBtn = document.getElementById('Addbook')
+const cancelButton = document.querySelector(".cancel")
+const AddBookBtn = document.querySelector(".add-book")
+const BookInput = document.querySelector(".book-input")
 
 
-let myLibrary = [1,2,3,4]
+let myLibrary = []
 
 
-
-function Book(title,author,pages,isRead){
-    this.title = title
-    this.author = author
-    this.numberOfPages = pages
-    this.isRead = isRead
-}
-
-const firstBook = new Book("old book", "alex robinett", 69, true)
-
-function addBookToLibrary(){
-    
+// constructor function
+function Book(bookTitle, bookAuthor, bookPages, isBookRead){
+    this.title = bookTitle
+    this.author = bookAuthor
+    this.numberOfPages = bookPages
+    this.isRead = isBookRead
 }
 
 
-
-function SetupBook(){
-    const Booktemplate = document.createElement("div")
-    Booktemplate.classList.add("card")
-    Booktemplate.classList.add("book")
-    Booktemplate.innerHTML = `<h2>Book Title:<span class="answer">${firstBook.title}</span></h2><span>Author:<span class="answer">${firstBook.author}</span></span><span class="pages">Number of pages:<span class="answer">${firstBook.numberOfPages}</span></span><button class="read-button">Read</button>`;
-    BookContainer[0].appendChild(Booktemplate)
+// get Radio button answer 
+function displayRadioValue() {
+    let ele = document.getElementsByName('read-unread');
+      
+    for(i = 0; i < ele.length; i++) {
+        if(ele[i].checked)
+            return ele[i].value
+        }
 }
+
+function resetCards(parent) {
+            while (parent.firstChild) {
+                parent.removeChild(parent.firstChild);
+            }
+}
+
+function clearInputs(){
+    const inputs = document.querySelectorAll('#booktitle, #author, #numpages');
+
+    inputs.forEach(input => {
+      input.value = '';
+    })
+}
+
+function toggleinputCard(){
+    document.querySelector(".book-form").classList.toggle("hidden")
+}
+        
+function addBookToLibrary(event){
+    // Add book to myLibrary array
+    const bookTitle = document.getElementById("booktitle").value
+    const bookAuthor = document.getElementById('author').value
+    const bookPages = document.getElementById("numpages").value
+    const isBookRead = displayRadioValue()
+    myLibrary.push(new Book(bookTitle,bookAuthor,bookPages,isBookRead))
+    console.log(myLibrary)
+    event.preventDefault()
+    resetCards(document.querySelector(".books-container"))
+    displayBooks()
+    BookInput.reset()
+    toggleinputCard()
+
+
+}
+
+
 
 function displayBooks(){
+    const BookContainer = document.getElementsByClassName("books-container")
     for(let i = 0; i < myLibrary.length; i++){
-       SetupBook()
+       const Booktemplate = document.createElement("div")
+       Booktemplate.dataset.num = i
+       Booktemplate.classList.add("card")
+       Booktemplate.classList.add("book")
+       Booktemplate.innerHTML = `<h2>Book Title:<span class="answer">${myLibrary[i].title}</span></h2><span>Author:<span class="answer">${myLibrary[i].author}</span></span><span class="pages">Number of pages:<span class="answer">${myLibrary[i].numberOfPages}</span></span><button class="${myLibrary[i].isRead}">Read</button>`;
+       BookContainer[0].appendChild(Booktemplate)
     }
 }
 
-displayBooks()
+BookFormBtn.addEventListener("click", toggleinputCard)
+
+cancelButton.addEventListener("click", toggleinputCard)
+
+AddBookBtn.addEventListener("click", addBookToLibrary, false)
